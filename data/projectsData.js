1eb37dcd4b93fb7a89e4a5f281ede9666ef453f3
Tamati Hoetawa <102144878+hoetawa@users.noreply.github.com>
2022-08-30 21:55:24 +0200

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,6 +1,12 @@
 const projectsData = [
   {
-    title: 'Auditing',
+    title: 'GDPR Set Up',
+    description: `A description of policy writing`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'Data Auditing',
     description: `A description of auditing services`,
     imgSrc: '/static/images/google.png',
     href: '/blog/danske-bank-is-fined',
@@ -8,6 +14,18 @@ const projectsData = [
   {
     title: 'Policy Writing',
     description: `A description of policy writing`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'Data Deletion',
+    description: `A description of data deletion`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'GDPR Training',
+    description: `A description of gdpr training`,
     imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
   },
